371d4611a1a5ca1034280364cdf4eef87f9835e7
Vincent Wochnik <v.wochnik@gmail.com>
2025-10-05 16:50:22 +0200

    fix

--- projects/ngx-waterbox/src/lib/theme.ts
+++ projects/ngx-waterbox/src/lib/theme.ts
@@ -12,7 +12,7 @@ type ThemePropertyMap = {
       : Theme[K] extends Pattern
       ? "pattern"
       : never;
-    default: Theme[K];
+    default: () => Theme[K];
     variable: string;
   };
 };
@@ -23,133 +23,133 @@ const THEME_PROPERTIES: ThemeProperty[] = [
     {
         key: "backFillColor",
         type: "string",
-        default: "rgb(100,130,160)",
+        default: () => "rgb(100,130,160)",
         variable: "back-fill-color"
     },
     {
         key: "backFillColorLight",
         type: "string",
-        default: "rgb(140,170,200)",
+        default: () => "rgb(140,170,200)",
         variable: "back-fill-color-light"
     },
     {
         key: "backFillColorDark",
         type: "string",
-        default: "rgb(70,100,130)",
+        default: () => "rgb(70,100,130)",
         variable: "back-fill-color-dark"
     },
     {
         key: "backStrokeColor",
         type: "string",
-        default: "rgb(30,40,50)",
+        default: () => "rgb(30,40,50)",
         variable: "back-stroke-color"
     },
     {
         key: "backPattern",
         type: "pattern",
-        default: {
+        default: () => ({
             name: 'none',
             size: 0,
             alpha: 0.5
-        },
+        }),
         variable: "back-pattern"
     },
     {
         key: "frontFillColor",
         type: "string",
-        default: "rgba(100,130,160,0.2)",
+        default: () => "rgba(100,130,160,0.2)",
         variable: "front-fill-color"
     },
     {
         key: "frontFillColorLight",
         type: "string",
-        default: "rgba(140,170,200,0.2)",
+        default: () => "rgba(140,170,200,0.2)",
         variable: "front-fill-color-light"
     },
     {
         key: "frontFillColorDark",
         type: "string",
-        default: "rgba(70,100,130,0.2)",
+        default: () => "rgba(70,100,130,0.2)",
         variable: "front-fill-color-dark"
     },
     {
         key: "frontStrokeColor",
         type: "string",
-        default: "rgba(30,40,50,0.2)",
+        default: () => "rgba(30,40,50,0.2)",
         variable: "front-stroke-color"
     },
     {
         key: "frontPattern",
         type: "pattern",
-        default: {
+        default: () => ({
             name: 'none',
             size: 0,
             alpha: 0.5
-        },
+        }),
         variable: "front-stroke-color"
     },
     {
         key: "waterFillColor",
         type: "string",
-        default: "rgba(10,60,120,0.7)",
+        default: () => "rgba(10,60,120,0.7)",
         variable: "water-fill-color"
     },
     {
         key: "waterFillColorLight",
         type: "string",
-        default: "rgba(30,90,180,0.7)",
+        default: () => "rgba(30,90,180,0.7)",
         variable: "water-fill-color-light"
     },
     {
         key: "waterFillColorDark",
         type: "string",
-        default: "rgba(0,40,80,0.7)",
+        default: () => "rgba(0,40,80,0.7)",
         variable: "water-fill-color-dark"
     },
     {
         key: "waterStrokeColor",
         type: "string",
-        default: "rgba(0,20,40,0.8)",
+        default: () => "rgba(0,20,40,0.8)",
         variable: "water-stroke-color"
     },
     {
         key: "waterPattern",
         type: "pattern",
-        default: {
+        default: () => ({
             name: 'none',
             size: 0,
             alpha: 0.5
-        },
+        }),
         variable: "water-pattern"
     },
     {
         key: "strokeWidth",
         type: "number",
-        default: 0.5,
+        default: () => 0.5,
         variable: "stroke-width"
     },
     {
         key: "divisions",
         type: "number",
-        default: 5,
+        default: () => 5,
         variable: "divisions"
     },
     {
         key: "separatorSize",
         type: "number",
-        default: 10,
+        default: () => 10,
         variable: "separator-size"
     },
     {
         key: "clipEdges",
         type: "boolean",
-        default: false,
+        default: () => false,
         variable: "clip-edges"
     },
     {
         key: "drawFront",
         type: "boolean",
-        default: false,
+        default: () => false,
         variable: "draw-front"
     }
 ];
@@ -158,7 +158,7 @@ export function getDefaultTheme(): Theme {
     return THEME_PROPERTIES.reduce((theme, property) => {
         return {
             ...theme,
-            [property.key]: property.default
+            [property.key]: property.default()
         };
     }, {} as Theme);
 }
